61098a842d4d788056ce39d9e773fc88751af9b7
Nick Overacker <nick.overacker@okstate.edu>
2020-06-20 21:27:32 +0900

    Update katakanizer.js

--- js/katakanizer.js
+++ js/katakanizer.js
@@ -2,44 +2,48 @@
 function convert()
 {
   let input = document.getElementById('katakanizer-input').value;
-  let placeholder = document.getElementById('katakanizer-input').placeholder;
   let output = "";
-
-  // Case 1: User input
-  if(input.length > 0)
-  {
-    let words = input.split(" ");
-    let punctuation = "";
-    
-    // The input is split by space characters.
-    // Convert each word individually.
-    // Allow ONE period or comma after each word.
-    for(let ii = 0; ii < words.length; ii++) {
-      // Comma after word?
-      if(words[ii].charAt(words[ii].length-1) === ',') {
-        punctuation = "、";
-        words[ii] = words[ii].substring(0,words[ii].length-1);
-      }
-      // Period after word?
-      else if(words[ii].charAt(words[ii].length-1) === '.') {
-        punctuation = "。";
-        words[ii] = words[ii].substring(0,words[ii].length-1);
-      }
-      
-      // Add to the output string.
-      output += latinToKana(words[ii]) + punctuation;
-      
-      // Add a space if necessary.
-      if(words.length - 1 > ii) {
-        output += "　";
-      }
-    } // end for loop
-  } // end case 1 (user input)
-  // Case 2: No user input (use placeholder).
-  else
-  {
-    output = latinToKana(placeholder);
+  let punctuation = "";
+  
+  // If no user input, use the placeholder.
+  if(input.length <= 0) {
+    input = document.getElementById('katakanizer-input').placeholder;
   }
+  
+  // Split the input into individual words.
+  let words = input.split(" ");
+
+  // The input is split by space characters.
+  // Convert each word individually.
+  // Allow ONE period, comma, or question mark after each word.
+  for(let ii = 0; ii < words.length; ii++) {
+    // Ignore empty strings.
+    if(words[ii].length === 0) continue;
+    
+    // Handle ONE punctuation mark per word.
+    switch(words[ii].charAt(words[ii].length-1) {
+           case ',':
+              punctuation = "、";
+              words[ii] = words[ii].substring(0,words[ii].length-1);
+              break;
+           case '.':
+              punctuation = "。";
+              words[ii] = words[ii].substring(0,words[ii].length-1);
+              break;
+           case '?':
+              punctuation = "？";
+              words[ii] = words[ii].substring(0,words[ii].length-1);
+              break;
+    }
+
+    // Add to the output string.
+    output += latinToKana(words[ii]) + punctuation;
+
+    // Add a space if necessary.
+    if(words.length - 1 > ii) {
+      output += "　";
+    }
+  } // end for loop
   
   // Final output
   document.getElementById("katakanizer-output").value = output;
